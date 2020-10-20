854ec1f8d897e55e8d40be6600c50f289efe2e0f
Amoko Ivan <amokoivan@gmail.com>
2020-10-20 11:33:17 +0300

    removed

--- fms-services/src/main/java/org/pahappa/systems/core/dao/WithdrawDao.java
+++
@@ -1,9 +0,0 @@
-package org.pahappa.systems.core.dao;
-
-
-import org.pahappa.systems.models.Withdraw;
-import org.sers.webutils.server.core.dao.BaseDao;
-
-public interface WithdrawDao extends BaseDao<Withdraw> {
-
-}
--- fms-services/src/main/java/org/pahappa/systems/core/dao/impl/EmailLogDao.java
+++
@@ -1,9 +0,0 @@
-package org.pahappa.systems.core.dao.impl;
-
-
-import org.pahappa.systems.models.Email;
-import org.sers.webutils.server.core.dao.BaseDao;
-
-public interface EmailLogDao extends BaseDao<Email>{
-
-}
